6f4b1770d4e05efe3947e09d999d05e920202897
wfellis <wfellis@btinternet.com>
2022-11-03 21:17:50 +0000

    buttons complete with redirect - Heading and paragraph in correct spot

---
+++ ButtonPress.js
@@ -0,0 +1,6 @@
+document.getElementById("JavaScript-Maze-Game").onclick = function () {
+    window.open('https://github.com/wfelliss/NEA', '_blank');
+}
+document.getElementById("Personal-Website").onclick = function () {
+    window.open('https://github.com/wfelliss/PersonalWebsite', '_blank');
+}
